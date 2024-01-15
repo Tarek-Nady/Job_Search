01d9b5a2f47d3f4194d3cf3d61cd6a213b3ba5bd
Tarek_Nady <34241000+Tarek-Nady@users.noreply.github.com>
2024-01-16 00:41:25 +0200

    Update DeleteJob.java

--- src/main/java/com/tarek/job_search/controller/DeleteJob.java
+++ src/main/java/com/tarek/job_search/controller/DeleteJob.java
@@ -14,8 +14,21 @@ import java.sql.SQLException;
 
 import static com.tarek.job_search.database.DbConnection.getConnection;
 
+/**
+ * Servlet implementation for deleting a job.
+ * Mapped to "/delete" URL pattern.
+ */
 @WebServlet("/delete")
 public class DeleteJob extends HttpServlet {
+     /**
+     * Handles the HTTP POST request.
+     * Receives the job ID from the request and attempts to delete the corresponding job from the database.
+     * 
+     * @param req  HttpServletRequest object that contains the request the client has made of the servlet
+     * @param resp HttpServletResponse object that contains the response the servlet sends to the client
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException      if an I/O error occurs
+     */
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
