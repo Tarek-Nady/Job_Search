a5ca54d7b63936a1c3719f95a73035572b4ce3f0
Tarek_Nady <34241000+Tarek-Nady@users.noreply.github.com>
2024-01-16 00:46:28 +0200

    Update UpdateJob.java

--- src/main/java/com/tarek/job_search/controller/UpdateJob.java
+++ src/main/java/com/tarek/job_search/controller/UpdateJob.java
@@ -14,11 +14,26 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Date;
 
+/**
+ * Servlet implementation for handling job updates.
+ * Mapped to "/edit_job" URL pattern.
+ */
 @WebServlet("/edit_job")
 public class UpdateJob extends HttpServlet {
+
+       /**
+     * Handles the HTTP POST request for updating a job.
+     * Receives job details from the request, updates the job information, and saves the changes to the database.
+     * 
+     * @param req  HttpServletRequest object that contains the request the client has made of the servlet
+     * @param resp HttpServletResponse object that contains the response the servlet sends to the client
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException      if an I/O error occurs
+     */
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
+             // Extract job details from request parameters
             int id = Integer.parseInt(req.getParameter("id"));
             String title = req.getParameter("title");
             String description = req.getParameter("description");
@@ -26,6 +41,7 @@ public class UpdateJob extends HttpServlet {
             String category = req.getParameter("category");
             String  pdata = req.getParameter("pdate");
             String status = req.getParameter("status");
+            // Create a JobDao instance for database operations
             JobDao dao = new JobDao(DbConnection.getConnection());
             Job job = new Job();
             job.setId(id);
